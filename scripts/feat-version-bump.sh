55ec8515128d9f7212a110485fca466f8ee739a4
smazik02 <s.mazik2002@gmail.com>
2024-11-26 21:39:01 +0100

    feat: :tada: skrypty do automatycznego wersjonowania

---
+++ scripts/feat-version-bump.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function get_mvn_project_version() {
+  ./mvnw -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec
+}
+
+if [[ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]]; then
+  printf "Don\'t run this script in main!"
+  exit 1
+fi
+
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "There are uncommited/untracked files in the repository"
+  git status -s
+  echo "Please save and commit changes before continuing"
+  exit 1
+fi
+
+cd ..
+
+feature_branch="$(git rev-parse --abbrev-ref HEAD)"
+git checkout -q main
+proj_ver=$(get_mvn_project_version)
+git checkout -q "$feature_branch"
+
+old_maj_ver="$(cut -d '.' -f 1 <<< "$proj_ver")"
+old_min_ver="$(cut -d '.' -f 2 <<< "$proj_ver")"
+new_proj_ver="$old_maj_ver.$((old_min_ver+1))"
+
+read -rp "Current project version is $proj_ver, update to $new_proj_ver? [y/N] " response
+if ! [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  echo "Operation cancelled"
+  exit 1
+fi
+
+./mvnw versions:set -q -Dexec.executable="echo" -DnewVersion="$new_proj_ver"
+rm pom.xml.versionsBackup
+echo "Successfully bumped version to $new_proj_ver!"
+
+git add pom.xml
+git commit -qm "Podbito wersję do $new_proj_ver"
+git push -q
---
+++ scripts/major-release.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+function get_mvn_project_version() {
+  ./mvnw -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec
+}
+
+if ! [[ "$(git rev-parse --abbrev-ref HEAD)" = "main" ]]; then
+  printf "You can only run the script in main!"
+  exit 1
+fi
+
+if [[ -n "$(git status --porcelain)" ]]; then
+  echo "There are uncommited/untracked files in the repository"
+  git status -s
+  echo "Please save and commit changes before continuing"
+  exit 1
+fi
+
+cd ..
+
+proj_ver=$(get_mvn_project_version)
+
+old_maj_ver="$(cut -d '.' -f 1 <<< "$proj_ver")"
+new_proj_ver="$((old_maj_ver+1)).0"
+
+read -rp "Current project version is $proj_ver, update to $new_proj_ver? [y/N] " response
+if ! [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  echo "Operation cancelled"
+  exit 1
+fi
+
+./mvnw versions:set -q -Dexec.executable="echo" -DnewVersion="$new_proj_ver"
+rm pom.xml.versionsBackup
+echo "Successfully bumped version to $new_proj_ver!"
+
+git add pom.xml
+git commit -qm "Nowy release: $new_proj_ver"
+git push -q
+
+git tag -a v"$new_proj_ver" -m "v$new_proj_ver"
+git push --tags -q
