42f6c6ba812c206c657d233a2de96361f069840c
Stanisław Mazik <76518169+smazik02@users.noreply.github.com>
2024-12-08 22:58:32 +0100

    [T01b] Zmiana Obiektów
    * chore: :recycle: changed method parameters and return types from String to DTO
    
    * Podbito wersję do 0.4

--- src/main/java/pl/put/poznan/jsontools/JsonToolsService.java
+++ src/main/java/pl/put/poznan/jsontools/JsonToolsService.java
@@ -1,15 +1,16 @@
 package pl.put.poznan.jsontools;
 
 import org.springframework.stereotype.Service;
+import pl.put.poznan.jsontools.types.JsonDto;
 
 @Service
 public class JsonToolsService {
 
-    public String minify(String inputJson) {
-        return inputJson.toLowerCase();
+    public JsonDto minify(JsonDto inputJson) {
+        return new JsonDto(inputJson.jsonString().toLowerCase());
     }
 
-    public String format(String inputJson) {
-        return inputJson.toUpperCase();
+    public JsonDto format(JsonDto inputJson) {
+        return new JsonDto(inputJson.jsonString().toUpperCase());
     }
 }
--- src/main/java/pl/put/poznan/jsontools/jsonmapper/JsonMapper.java
+++ src/main/java/pl/put/poznan/jsontools/jsonmapper/JsonMapper.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
+import pl.put.poznan.jsontools.types.JsonDto;
 import pl.put.poznan.jsontools.types.JsonObject;
 
 import java.util.Map;
@@ -12,12 +13,12 @@ import java.util.Map;
 public class JsonMapper {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public JsonObject toJsonObject(String jsonString) throws JsonProcessingException {
-        Map<String, Object> jsonValues = objectMapper.readValue(jsonString, new TypeReference<>() {});
+    public JsonObject toJsonObject(JsonDto jsonDto) throws JsonProcessingException {
+        Map<String, Object> jsonValues = objectMapper.readValue(jsonDto.jsonString(), new TypeReference<>() {});
         return new JsonObject(jsonValues);
     }
 
-    public String toJsonString(JsonObject jsonObject) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(jsonObject.getValues());
+    public JsonDto toJsonDto(JsonObject jsonObject) throws JsonProcessingException {
+        return new JsonDto(objectMapper.writeValueAsString(jsonObject.getValues()));
     }
 }
