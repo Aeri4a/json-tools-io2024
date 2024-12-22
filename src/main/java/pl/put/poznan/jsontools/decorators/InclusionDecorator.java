70f6f6775863966e688771fed41edf17220f134f
hkereb <115366640+hkereb@users.noreply.github.com>
2024-12-22 21:49:12 +0100

    docs: :memo: add javadoc comments to decorator classes

--- src/main/java/pl/put/poznan/jsontools/decorators/InclusionDecorator.java
+++ src/main/java/pl/put/poznan/jsontools/decorators/InclusionDecorator.java
@@ -2,17 +2,27 @@ package pl.put.poznan.jsontools.decorators;
 
 import pl.put.poznan.jsontools.exceptions.InvalidInputException;
 import pl.put.poznan.jsontools.types.IJsonObject;
+import pl.put.poznan.jsontools.types.JsonObject;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Implementuje konkretny dekorator, który filtruje dane JSON, zostawiając jedynie wskazane klucze <code>keys</code>.
+ * <p>
+ * Stosowany do obiektu {@link JsonObject} (implementującego interfejs {@link IJsonObject}),
+ * a jego zadaniem jest zachowanie z niego wartości związanych z określonymi kluczami.
+ */
 public class InclusionDecorator extends JsonDecorator {
     public InclusionDecorator(IJsonObject jsonObject, List<String> keys) {
         super(jsonObject, keys);
         filter();
     }
 
+    /**
+     * Filtruje obiekt JSON, zostawiając w nim tylko wartości skojarzone z podanymi kluczami z mapy.
+     */
     private void filter() {
         Map<String, Object> map = this.jsonObject.getValues();
         Map<String, Object> filteredMap = new HashMap<>();
--- src/main/java/pl/put/poznan/jsontools/types/JsonDto.java
+++ src/main/java/pl/put/poznan/jsontools/types/JsonDto.java
@@ -8,8 +8,8 @@ import java.util.List;
  * Główny obiekt DTO przychodzący do aplikacji i z niej wychodzący
  *
  * @param jsonString pole wymagane zawierające łańcuch znaków do dalszego przetwarzania
- * @param includeKeys pole opcjonalne zawierające liste wartości kluczy które mają być uwzględniane przy wyniku
- * @param excludeKeys pole opcjonalne zawierające liste wartości kluczy które nie mają być uwzględniane przy wyniku
+ * @param includeKeys pole opcjonalne zawierające listę wartości kluczy, które mają być uwzględniane przy wyniku
+ * @param excludeKeys pole opcjonalne zawierające listę wartości kluczy, które nie mają być uwzględniane przy wyniku
  */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record JsonDto(String jsonString, List<String> includeKeys, List<String> excludeKeys) {
