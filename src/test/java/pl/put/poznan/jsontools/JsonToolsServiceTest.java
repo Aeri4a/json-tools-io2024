9886347cfb6ab2f85b24d09ceb1937a1b5918e09
Anna Gąsiorowska <80831299+Aeri4a@users.noreply.github.com>
2024-12-22 22:58:46 +0100

    [T22] Compare function tests
    test: :white_check_mark: added tests to compare method & one fix

--- src/test/java/pl/put/poznan/jsontools/JsonToolsServiceTest.java
+++ src/test/java/pl/put/poznan/jsontools/JsonToolsServiceTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.put.poznan.jsontools.exceptions.InvalidInputException;
 import pl.put.poznan.jsontools.jsonmapper.JsonMapper;
+import pl.put.poznan.jsontools.types.InputCompareDto;
 import pl.put.poznan.jsontools.types.JsonDto;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -69,4 +72,48 @@ public class JsonToolsServiceTest {
         verify(mockJsonMapper, times(1)).toJsonDtoWithFormat(any());
     }
 
+    @Test
+    void compareShouldReturnEmptyListIfTwoStringAreEqual() {
+        var input = new InputCompareDto("hello", "hello");
+
+        var jsonToolsService = new JsonToolsService();
+
+        assertEquals(0, jsonToolsService.compare(input).differentLines().size());
+    }
+
+    @Test
+    void compareShouldReturnOneDifferenceOneLineEqualLength() {
+        var input = new InputCompareDto("olleh", "hello");
+
+        var jsonToolsService = new JsonToolsService();
+
+        assertEquals(List.of(1), jsonToolsService.compare(input).differentLines());
+    }
+
+    @Test
+    void compareShouldReturnOneDifferenceIfOneLineIsEmpty() {
+        var input = new InputCompareDto("abc\ncba", "\ncba");
+
+        var jsonToolsService = new JsonToolsService();
+
+        assertEquals(List.of(1), jsonToolsService.compare(input).differentLines());
+    }
+
+    @Test
+    void compareShouldReturnTwoDifferencesIfLinesAreEmptyAlternately() {
+        var input = new InputCompareDto("\ncba", "cba\n");
+
+        var jsonToolsService = new JsonToolsService();
+
+        assertEquals(List.of(1, 2), jsonToolsService.compare(input).differentLines());
+    }
+
+    @Test
+    void compareShouldReturnEmptyListIfTwoStringsAreEmpty() {
+        var input = new InputCompareDto(null, null);
+
+        var jsonToolsService = new JsonToolsService();
+
+        assertThrows(InvalidInputException.class, () -> jsonToolsService.compare(input));
+    }
 }
