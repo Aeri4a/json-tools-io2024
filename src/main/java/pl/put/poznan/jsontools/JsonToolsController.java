f7e4b3f2ee4a0404e32108efabc78efb8e3907dc
Hanna Kerebińska <115366640+hkereb@users.noreply.github.com>
2024-12-22 20:43:15 +0100

    [T17] Compare Functionality  (#55)
    * feat: :sparkles: add a function to compare two strings + required DTO objects
    
    * Podbito wersję do 1.1

--- src/main/java/pl/put/poznan/jsontools/JsonToolsController.java
+++ src/main/java/pl/put/poznan/jsontools/JsonToolsController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.put.poznan.jsontools.types.InputCompareDto;
 import pl.put.poznan.jsontools.types.JsonDto;
+import pl.put.poznan.jsontools.types.OutputCompareDto;
 
 @RestController
 @RequestMapping("/json")
@@ -39,5 +41,15 @@ public class JsonToolsController {
         JsonDto formattedJson = jsonToolsService.format(inputJson);
         logger.debug("formatted JSON:\n{}", formattedJson.jsonString());
         return ResponseEntity.ok(formattedJson);
+    }
+
+    @PostMapping("/compare")
+    public ResponseEntity<OutputCompareDto> compareJson(@RequestBody InputCompareDto inputJsons, HttpServletRequest request) {
+        logger.info("handled request at {}", request.getRequestURL().toString());
+        logger.debug("request body at compareJson: json1={}, json2={}", inputJsons.string1(), inputJsons.string2());
+
+        OutputCompareDto comparisonResult = jsonToolsService.compare(inputJsons);
+        logger.debug("comparison result: {}", comparisonResult);
+        return ResponseEntity.ok(comparisonResult);
     }
 }
